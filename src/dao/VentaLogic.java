9983114896378c22839f8869cc8b1e1680dfbbd9
Anner Alessandro Teruel Pineda <aateruel@uth.hn>
2026-04-07 18:42:16 -0600

    Primer Commit: Creacion de Clases Creacion de 4 JFrameForm

---
+++ src/dao/VentaLogic.java
@@ -0,0 +1,12 @@
+package dao;
+
+import config.Conexion;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class VentaLogic {
+
+    
+    
+}
